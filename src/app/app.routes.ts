fad712bfe19b11b316036bd830e5e042e911ac95
Carlos Andres Benavides <cabenavidesd@misena.edu.co>
2019-01-02 17:36:18 -0500

    style: formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 
 const ROUTES: Routes = [
-    { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'search', component: SearchComponent },
     { path: 'artist/:id', component: ArtistComponent },
+    { path: '', component: HomeComponent },
     { path: '**', component: HomeComponent },
 ];
 
