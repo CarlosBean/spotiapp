daffbc3e428213f634905c94258bddcad3a60fe2
Carlos Andres Benavides <cabenavidesd@misena.edu.co>
2019-01-02 17:31:31 -0500

    chore: add enviroment constants

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_url: 'https://api.spotify.com/v1',
+  token_url: 'https://accounts.spotify.com/api/token',
+  audio_url: 'https://open.spotify.com/embed?uri=',
+  client_id: '6a44fd2abdd54652a6bf2ae6c33404bf',
+  client_secret: 'b510f9923b20433ea7ee43e033c6136b',
 };
